16019bc73c202556f57092ac63ea37b876418b28
iam-mohan-palani <pmohankumar29@gmail.com>
2023-12-12 14:55:08 +0530

    First commit

--- First.py
+++ First.py
@@ -1,1 +1,13 @@
-print("Hello Mohankumar Palaniappan")
+# Start your Python journey here
+
+#Fundamentals
+
+#1 Print
+print("Hello, Mohankumar Palaniappan \nWelcome ! to the world of Pyhton!!!")
+
+print("Hello, Mohankumar Palaniappan ", end="")
+print("Welcome ! to the world of Pyhton!!!")
+
+print("Hello, Mohankumar Palaniappan ")
+print("Welcome ! to the world of Pyhton!!!")
+
